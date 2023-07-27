e10237e25ad810615974ba3801cc59bc034d33d1
Bhargavi-stanleymldl <66639416+Bhargavi-stanleymldl@users.noreply.github.com>
2023-07-27 13:33:28 +0530

    Create protection_rules.py

---
+++ protection_rules.py
@@ -0,0 +1,16 @@
+import os
+from github import Github
+
+def enable_branch_protection(repo_owner, repo_name, branch_name, access_token):
+    g = Github(access_token)
+    repo = g.get_repo(f"{repo_owner}/{repo_name}")
+    branch = repo.get_branch(branch_name)
+    branch.edit_protection(required_approving_review_count=1, dismiss_stale_reviews=True)
+
+# Example usage
+repo_owner = "Bhargavi-stanleymldl"
+repo_name = "automation-repo"
+branch_name = "develop"
+github_access_token = os.environ.get('GITLAB_ACCESS_TOKEN')
+
+enable_branch_protection(repo_owner, repo_name, branch_name, github_access_token)
